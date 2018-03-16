fd4b2ae833672fb9ebd68a996373e5d484bd4ab8
lcdtyph <lcdtyph@gmail.com>
2018-03-17 00:37:54 +0800

    add CryptoContext

--- crypto_utils/include/crypto_utils/cipher.h
+++ crypto_utils/include/crypto_utils/cipher.h
@@ -1,6 +1,10 @@
 #ifndef __CIPHER_H__
 #define __CIPHER_H__
 
+#include <utility>
+#include <memory>
+#include <type_traits>
+
 #include <common_utils/buffer.h>
 
 class Cipher {
@@ -13,6 +17,35 @@ public:
 
 private:
 };
+
+class CryptoContext {
+public:
+    virtual ssize_t Decrypt(Buffer &buf) = 0;
+    virtual ssize_t Encrypt(Buffer &buf) = 0;
+};
+
+template<typename CipherType>
+class __HelperCryptoContext : public CryptoContext {
+    static_assert(std::is_base_of<Cipher, CipherType>::value, "The cipher type must inherit from Cipher");
+public:
+    template<typename ...Args>
+        __HelperCryptoContext(Args&& ...args)
+            : decryptor_(new CipherType(std::forward<Args>(args)...)),
+              encryptor_(new CipherType(std::forward<Args>(args)...)) {
+        }
+
+    ssize_t Decrypt(Buffer &buf) { return decryptor_->Decrypt(buf); }
+    ssize_t Encrypt(Buffer &buf) { return encryptor_->Encrypt(buf); }
+
+private:
+    std::unique_ptr<Cipher> decryptor_;
+    std::unique_ptr<Cipher> encryptor_;
+};
+
+template<typename CipherType, typename ...Args>
+std::unique_ptr<CryptoContext> GetCryptoContext(Args&& ...args) {
+    return std::make_unique<__HelperCryptoContext<CipherType>>(std::forward<Args>(args)...);
+}
 
 #endif
 
