b013e8af678c151c038af7b1feff3fc208bf35f0
lcdtyph <lcdtyph@gmail.com>
2018-03-18 04:04:08 +0800

    add aes-256-gcm

--- crypto_utils/src/aes_gcm_family.cc
+++ crypto_utils/src/aes_gcm_family.cc
@@ -4,13 +4,13 @@
 #include "crypto_utils/crypto.h"
 #include "crypto_utils/aead.h"
 
-class Chacha20Poly1305Ietf final : public AeadCipher<32, 12, 16> {
+class Aes256Gcm final : public AeadCipher<32, 12, 16> {
 public:
-    Chacha20Poly1305Ietf(std::vector<uint8_t> master_key)
+    Aes256Gcm(std::vector<uint8_t> master_key)
         : AeadCipher(std::move(master_key)) {
     }
 
-    ~Chacha20Poly1305Ietf() { }
+    ~Aes256Gcm() { }
 
 private:
     int CipherEncrypt(
@@ -28,7 +28,7 @@ private:
         );
 };
 
-int Chacha20Poly1305Ietf::CipherEncrypt(
+int Aes256Gcm::CipherEncrypt(
                 void *c, size_t *clen,
                 const uint8_t *m, size_t mlen,
                 const uint8_t *ad, size_t adlen,
@@ -36,16 +36,16 @@ int Chacha20Poly1305Ietf::CipherEncrypt(
     ) {
     int ret;
     unsigned long long clenll;
-    ret = crypto_aead_chacha20poly1305_ietf_encrypt(
-                    (uint8_t *)c, &clenll,
-                    m, mlen, ad, adlen,
-                    nullptr, n, k
+    ret = crypto_aead_aes256gcm_encrypt(
+                (uint8_t *)c, &clenll,
+                m, mlen, ad, adlen,
+                nullptr, n, k
           );
     *clen = (size_t)clenll;
     return ret;
 }
 
-int Chacha20Poly1305Ietf::CipherDecrypt(
+int Aes256Gcm::CipherDecrypt(
                 void *m, size_t *mlen,
                 const uint8_t *c, size_t clen,
                 const uint8_t *ad, size_t adlen,
@@ -53,7 +53,7 @@ int Chacha20Poly1305Ietf::CipherDecrypt(
     ) {
     int ret;
     unsigned long long mlenll;
-    ret = crypto_aead_chacha20poly1305_ietf_decrypt(
+    ret = crypto_aead_aes256gcm_decrypt(
                     (uint8_t *)m, &mlenll,
                     nullptr,
                     c, clen, ad, adlen,
@@ -63,5 +63,5 @@ int Chacha20Poly1305Ietf::CipherDecrypt(
     return ret;
 }
 
-static const CryptoContextGeneratorRegister<Chacha20Poly1305Ietf> kReg("chacha20-ietf-poly1305");
+static const CryptoContextGeneratorRegister<Aes256Gcm> kReg("aes-256-gcm");
 
