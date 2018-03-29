3199abf492d25ba6d6c0160ffcf1a5b4e61ccabf
lcdtyph <lcdtyph@gmail.com>
2018-03-29 22:38:50 +0800

    now depends on openssl 1.0.2 instead of openssl 1.1

--- shadowsocks/crypto_utils/src/cipher.cc
+++ shadowsocks/crypto_utils/src/cipher.cc
@@ -1,27 +1,23 @@
 
 #include <openssl/evp.h>
-#include <openssl/kdf.h>
+#include <openssl/hmac.h>
 
 #include "crypto_utils/cipher.h"
+ 
+static uint8_t *HKDF(const EVP_MD *evp_md,
+                     const uint8_t *salt, size_t salt_len,
+                     const uint8_t *key, size_t key_len,
+                     const uint8_t *info, size_t info_len,
+                     uint8_t *okm, size_t okm_len);
 
 bool Cipher::HKDF_SHA1(const uint8_t *key, size_t key_len,
                        const uint8_t *salt, size_t salt_len,
                        const uint8_t *info, size_t info_len,
                        uint8_t *session_key, size_t skey_len) {
-    bool result = true;
-    EVP_PKEY_CTX *ctx;
-    size_t outlen = skey_len;
-
-    ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, nullptr);
-    result = (EVP_PKEY_derive_init(ctx) > 0)
-          && (EVP_PKEY_CTX_set_hkdf_md(ctx, EVP_sha1()) > 0)
-          && (EVP_PKEY_CTX_set1_hkdf_salt(ctx, salt, salt_len) > 0)
-          && (EVP_PKEY_CTX_set1_hkdf_key(ctx, key, key_len) > 0)
-          && (EVP_PKEY_CTX_add1_hkdf_info(ctx, info, info_len) > 0)
-          && (EVP_PKEY_derive(ctx, session_key, &outlen) > 0);
-    result = result && (outlen == skey_len);
-
-    return result;
+    return HKDF(EVP_sha1(), salt, salt_len,
+                key, key_len,
+                info, info_len,
+                session_key, skey_len) != nullptr;
 }
 
 static size_t __BytesToKey(const std::string &password, uint8_t *key, size_t key_len);
@@ -43,7 +39,7 @@ size_t __BytesToKey(const std::string &password, uint8_t *key, size_t key_len) {
     EVP_MD_CTX *ctx = EVP_MD_CTX_new();
     uint8_t md_buf[EVP_MAX_MD_SIZE];
     int addmd;
-    unsigned int i, j, mds;
+    uint32_t i, j, mds;
 
     mds = EVP_MD_size(md);
 
@@ -64,5 +60,97 @@ size_t __BytesToKey(const std::string &password, uint8_t *key, size_t key_len) {
 
     EVP_MD_CTX_free(ctx);
     return key_len;
+}
+
+static uint8_t *HKDF_Extract(const EVP_MD *evp_md,
+                             const uint8_t *salt, size_t salt_len,
+                             const uint8_t *key, size_t key_len,
+                             uint8_t *prk, size_t *prk_len);
+
+static uint8_t *HKDF_Expand(const EVP_MD *evp_md,
+                            const uint8_t *prk, size_t prk_len,
+                            const uint8_t *info, size_t info_len,
+                            uint8_t *okm, size_t okm_len);
+
+static uint8_t *HKDF(const EVP_MD *evp_md,
+                     const uint8_t *salt, size_t salt_len,
+                     const uint8_t *key, size_t key_len,
+                     const uint8_t *info, size_t info_len,
+                     uint8_t *okm, size_t okm_len)
+{
+    uint8_t prk[EVP_MAX_MD_SIZE];
+    uint8_t *ret;
+    size_t prk_len;
+
+    if (!HKDF_Extract(evp_md, salt, salt_len, key, key_len, prk, &prk_len))
+        return NULL;
+
+    ret = HKDF_Expand(evp_md, prk, prk_len, info, info_len, okm, okm_len);
+    OPENSSL_cleanse(prk, sizeof(prk));
+
+    return ret;
+}
+
+static uint8_t *HKDF_Extract(const EVP_MD *evp_md,
+                             const uint8_t *salt, size_t salt_len,
+                             const uint8_t *key, size_t key_len,
+                             uint8_t *prk, size_t *prk_len)
+{
+    uint32_t tmp_len;
+
+    if (!HMAC(evp_md, salt, salt_len, key, key_len, prk, &tmp_len))
+        return NULL;
+
+    *prk_len = tmp_len;
+    return prk;
+}
+
+static uint8_t *HKDF_Expand(const EVP_MD *evp_md,
+                            const uint8_t *prk, size_t prk_len,
+                            const uint8_t *info, size_t info_len,
+                            uint8_t *okm, size_t okm_len)
+{
+    HMAC_CTX *hmac;
+    uint32_t i;
+    uint8_t prev[EVP_MAX_MD_SIZE];
+    size_t done_len = 0, dig_len = EVP_MD_size(evp_md);
+    size_t n = okm_len / dig_len;
+
+    if (okm_len % dig_len)
+        n++;
+    if (n > 255 || okm == NULL)
+        return NULL;
+    if ((hmac = HMAC_CTX_new()) == NULL)
+        return NULL;
+    if (!HMAC_Init_ex(hmac, prk, prk_len, evp_md, NULL))
+        goto err;
+
+    for (i = 1; i <= n; i++) {
+        size_t copy_len;
+        const uint8_t ctr = i;
+        if (i > 1) {
+            if (!HMAC_Init_ex(hmac, NULL, 0, NULL, NULL))
+                goto err;
+            if (!HMAC_Update(hmac, prev, dig_len))
+                goto err;
+        }
+        if (!HMAC_Update(hmac, info, info_len))
+            goto err;
+        if (!HMAC_Update(hmac, &ctr, 1))
+            goto err;
+        if (!HMAC_Final(hmac, prev, NULL))
+            goto err;
+        copy_len = (done_len + dig_len > okm_len) ?
+                       okm_len - done_len :
+                       dig_len;
+        memcpy(okm + done_len, prev, copy_len);
+        done_len += copy_len;
+    }
+    HMAC_CTX_free(hmac);
+    return okm;
+
+ err:
+    HMAC_CTX_free(hmac);
+    return NULL;
 }
 
