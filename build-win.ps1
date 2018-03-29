f9c841216c7a6e42775bc2caf63e35bd5758f10c
lcdtyph <lcdtyph@gmail.com>
2018-03-29 20:14:08 +0800

    add windows build script

---
+++ build-win.ps1
@@ -0,0 +1,3 @@
+cmake -DVCPKG_TARGET_TRIPLET="${Env:VCPKG_DEFAULT_TRIPLET}" -DCMAKE_TOOLCHAIN_FILE="${Env:VCPKG_ROOT}\scripts\buildsystems\vcpkg.cmake" -DCMAKE_INSTALL_PREFIX="${Env:HOMEPATH}\Documents" -G "Visual Studio 15 2017 Win64"
+cmake --build simple-obfs --config Release
+
